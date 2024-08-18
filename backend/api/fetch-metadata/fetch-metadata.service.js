2794b283e810aa2bbaf118d0c1d60ff7c70decb0
Uri Baynesay <uri552@gmail.com>
2024-08-18 17:21:21 +0300

    remove waiting to response from fetching every url to start converting to text

--- backend/api/fetch-metadata/fetch-metadata.service.js
+++ backend/api/fetch-metadata/fetch-metadata.service.js
@@ -3,35 +3,21 @@ const { JSDOM } = jsdom
 
 function fetchMetaData(req, res) {
   const urls = req.body
-  const fetchedUrls = getUrlFetchPromises(urls)
-  Promise.all(fetchedUrls)
-    .then((values) => {
-      const htmlTexts = []
-      values.forEach((value) => {
-        htmlTexts.push(value.text())
+  const responsesToTexts = urls.map(url=>fetch(url).then(response=>response.text()))
+  Promise.all(responsesToTexts)
+    .then((htmlTexts) => {
+      result = htmlTexts.map((htmlText) => {
+        const dom = new JSDOM(htmlText)
+        const title = dom.window.document.title
+        const description = dom.window.document
+          .getElementsByTagName("meta")
+          .namedItem("description")?.content
+        const imageUrl = dom.window.document.images[0]?.src
+        return { title, description, imageUrl }
       })
-      Promise.all(htmlTexts).then((htmlTexts) => {
-        result=htmlTexts.map(htmlText=>{
-          const dom = new JSDOM(htmlText)
-          const title = dom.window.document.title
-          const description = dom.window.document
-            .getElementsByTagName("meta")
-            .namedItem("description")?.content
-          const imageUrl = dom.window.document.images[0]?.src
-          return { title, description, imageUrl }
-        })
-        res.send(result)
-      })
+      res.send(result)
     })
     .catch((error) => res.status(404).send(error))
-}
-
-function getUrlFetchPromises(urls) {
-  const fetchedUrls = []
-  urls.forEach((url) => {
-    if (url) fetchedUrls.push(fetch(url))
-  })
-  return fetchedUrls
 }
 
 module.exports = { fetchMetaData }
