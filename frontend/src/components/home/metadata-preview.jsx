924387e625623ef88f0471ad43b0548aafb97b11
Uri Baynesay <uri552@gmail.com>
2024-08-19 17:22:54 +0300

    styling of data preview

--- frontend/src/components/home/metadata-preview.jsx
+++ frontend/src/components/home/metadata-preview.jsx
@@ -15,10 +15,10 @@
 export const MetaDataPreview = ({ data }) => {
   return (
     <li className="metadata-preview-container bg-slate-50 shadow-md shadow-slate-25 rounded-xl mt-4 p-2 w-fit">
-      <article className="flex">
-        <div className="text-info w-40">
-          <h1 className="font-bold">Title : {data.title}</h1>
-          <p>Description : {data.description}</p>
+      <article className="flex gap-6">
+        <div className="text-info text-center w-40">
+          <h1 className="font-bold">{data.title}</h1>
+          <p>{data.description}</p>
         </div>
         <img className="max-h-24" src={data.imageUrl} alt="" />
       </article>
